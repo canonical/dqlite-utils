6f88fe79f80413cf2a58007192b0be9c95ebc206
Marco Manino <mrcmnn@gmail.com>
2025-10-09 09:40:32 +0200

    Simplify test

--- src/dqlite_sys.rs
+++ src/dqlite_sys.rs
@@ -14,10 +14,10 @@ fn test() {
     unsafe {
         let dir = CString::new(".").unwrap();
 
-        let mut snapshots = 0 as *mut dqlite_bindings::uvSnapshotInfo;
+        let mut snapshots = std::ptr::null_mut();
         let mut n_snapshots = 0usize;
 
-        let mut segments = 0 as *mut dqlite_bindings::uvSegmentInfo;
+        let mut segments = std::ptr::null_mut();
         let mut n_segments = 0usize;
 
         let mut errmsg = [0; dqlite_bindings::RAFT_ERRMSG_BUF_SIZE as usize];
