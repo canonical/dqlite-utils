d48032b66269cf905dc2bff492d1a1c50e01bca3
kcza <ed@kcza.net>
2025-11-05 14:20:09 +0100

    Add `status` command

---
+++ src/command/status.rs
@@ -0,0 +1,53 @@
+use anyhow::Result;
+use indoc::eprintdoc;
+
+use crate::{dqlite::DqliteSegment, Context};
+
+use super::UnrecognisedArgumentsError;
+
+#[derive(Debug)]
+pub(crate) struct Command;
+
+impl Command {
+    pub(crate) fn try_from_args(args: &[String]) -> Result<Self> {
+        if !args.is_empty() {
+            return Err(UnrecognisedArgumentsError(args.to_vec()).into());
+        }
+        Ok(Self)
+    }
+
+    pub(crate) fn run(&self, ctx: &Context) -> Result<()> {
+        let dqlite = &ctx.dqlite;
+
+        let last_closed_index = dqlite
+            .closed_segments()
+            .last()
+            .map(|segment| match segment {
+                DqliteSegment::Closed { indexes, .. } => indexes.end(),
+                _ => unreachable!(),
+            })
+            .cloned();
+        let num_entries_in_open_segments: u64 = dqlite
+            .open_segments()
+            .iter()
+            .map(|segment| match segment {
+                DqliteSegment::Open { counter, .. } => counter,
+                _ => unreachable!(),
+            })
+            .sum();
+        let first_index = dqlite.first_index();
+        let last_index = last_closed_index.unwrap_or(first_index) + num_entries_in_open_segments;
+
+        let dir = ctx.dir.display();
+        let term = dqlite.term();
+        eprintdoc!(
+            "
+                dir: {dir}
+                term: {term}
+                current_index: {last_index}
+                first_index: {first_index}
+            "
+        );
+        Ok(())
+    }
+}
