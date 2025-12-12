41e114e7beb0930562af9cfa6de74ab348d62394
kcza <ed@kcza.net>
2025-12-12 18:56:44 +0000

    Add `.finish` command

---
+++ src/command/snapshot/finish.rs
@@ -0,0 +1,118 @@
+use std::ffi::CString;
+use std::fs;
+use std::io::{ErrorKind, Write};
+use std::path::PathBuf;
+use std::time::SystemTime;
+
+use anyhow::{Context as _, anyhow};
+
+use crate::command::help::Help;
+use crate::command::snapshot::ShellSnapshotContext;
+use crate::command::{MissingArgumentError, UnrecognizedArgumentsError};
+use crate::dqlite::{DqliteDatabaseWriter, DqliteDir, RaftConfiguration};
+use crate::{Context, Result, Shell};
+
+#[derive(Debug)]
+pub(crate) struct FinishCommand {
+    dir: PathBuf,
+}
+
+impl FinishCommand {
+    pub(crate) fn help() -> Help {
+        Help::builder()
+            .name(".finish")
+            .summary("validate snapshot and write to disk")
+            .add_arg("dir", "the directory to save the snapshot into")
+            .build()
+            .expect("internal error: help invalid")
+    }
+
+    pub(crate) fn try_from_args(args: &[String]) -> Result<Self> {
+        let dir = match args {
+            [] => return Err(MissingArgumentError("dir").into()),
+            [dir] => dir,
+            [_, tail @ ..] => return Err(UnrecognizedArgumentsError(tail.to_vec()).into()),
+        };
+        let dir = PathBuf::from(dir);
+        Ok(Self { dir })
+    }
+
+    pub(crate) fn run(self, ctx: &mut Context) -> Result<()> {
+        let Self { dir } = self;
+        let shell = ctx.shell.snapshot().ok_or_else(|| {
+            anyhow!("internal error: .finish command not called in snapshot shell")
+        })?;
+
+        let ShellSnapshotContext {
+            term,
+            index,
+            timestamp,
+            configuration,
+        } = shell.snapshot.clone();
+        let timestamp = SystemTime::from(timestamp);
+        let configuration =
+            RaftConfiguration::try_from(configuration).context("cannot write snapshot")?;
+
+        // Heuristic to ensure clean directory. Clearly there's a TOCTOU issue here,
+        // but if a user chooses to write a snapshot into an actively-changing
+        // directory then on their head, be it.
+        match fs::read_dir(&dir) {
+            Ok(mut dir_reader) => {
+                if dir_reader.next().is_some() {
+                    return Err(anyhow!("directory not empty"))
+                        .with_context(|| anyhow!("cannot write snapshot into {}", dir.display()));
+                }
+            }
+            Err(err) if err.kind() == ErrorKind::NotFound => {}
+            Err(err) => {
+                return Err(err)
+                    .with_context(|| anyhow!("cannot write snapshot into {}", dir.display()));
+            }
+        }
+
+        DqliteDir::creator(dir)
+            .with_snapshot(move |s| {
+                s.with_term(term)
+                    .with_index(index)
+                    .with_timestamp(timestamp)
+                    .with_configuration(configuration)
+                    .add_database(
+                        CString::new("placeholder db".as_bytes().to_owned())
+                            .expect("internal error: CString invalid"),
+                        PlaceholderDb,
+                    )
+            })
+            .create()?;
+
+        ctx.shell = Shell::default();
+
+        Ok(())
+    }
+}
+
+struct PlaceholderDb;
+
+impl PlaceholderDb {
+    const MAIN_CONTENT: &str = "placeholder main";
+    const WAL_CONTENT: &str = "placeholder wal";
+}
+
+impl DqliteDatabaseWriter for PlaceholderDb {
+    fn main_size(&self) -> usize {
+        Self::MAIN_CONTENT.len()
+    }
+
+    fn wal_size(&self) -> usize {
+        Self::WAL_CONTENT.len()
+    }
+
+    fn write_main(&self, out: &mut impl Write) -> Result<()> {
+        write!(out, "{}", Self::MAIN_CONTENT)?;
+        Ok(())
+    }
+
+    fn write_wal(&self, out: &mut impl Write) -> Result<()> {
+        write!(out, "{}", Self::WAL_CONTENT)?;
+        Ok(())
+    }
+}
