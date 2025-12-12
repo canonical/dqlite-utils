34bd884ec3d5061cd1fbd3a3f2c84b6e81daeed5
kcza <ed@kcza.net>
2025-12-12 17:28:32 +0000

    Add `.set-term` command

---
+++ src/command/snapshot/set_term.rs
@@ -0,0 +1,42 @@
+use anyhow::{Context as _, anyhow};
+
+use crate::{
+    Context, Result,
+    command::{MissingArgumentError, UnrecognizedArgumentsError, help::Help},
+};
+
+#[derive(Debug)]
+pub(crate) struct SetTermCommand {
+    term: u64,
+}
+
+impl SetTermCommand {
+    pub(crate) fn help() -> Help {
+        Help::builder()
+            .name(".set-term")
+            .summary("set the term of the snapshot")
+            .build()
+            .expect("internal error: help invalid")
+    }
+
+    pub(crate) fn try_from_args(args: &[String]) -> Result<Self> {
+        let term = match args {
+            [] => return Err(MissingArgumentError("term").into()),
+            [term] => term,
+            [_, tail @ ..] => return Err(UnrecognizedArgumentsError(tail.to_vec()).into()),
+        };
+        let term = term
+            .parse()
+            .with_context(|| anyhow!("cannot parse term {term}"))?;
+        Ok(Self { term })
+    }
+
+    pub(crate) fn run(self, ctx: &mut Context) -> Result<()> {
+        let Self { term } = self;
+        let shell = ctx.shell.snapshot_mut().ok_or_else(|| {
+            anyhow!("internal error: .set-term command not called in snapshot shell")
+        })?;
+        shell.snapshot.term = term;
+        Ok(())
+    }
+}
