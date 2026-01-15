7001f8d2a4daac0e95361f12599cc38d1d25469d
kcza <ed@kcza.net>
2026-01-15 17:25:27 +0000

    Simplify prompt

--- src/prompt.rs
+++ src/prompt.rs
@@ -3,46 +3,37 @@ use std::fmt::Display;
 
 use owo_colors::Style;
 
-use crate::utils::TerminalStylizeExt;
-
 #[derive(Debug)]
 pub struct Prompt {
-    _content: Cow<'static, str>,
-    pretty_content: String,
+    content: Cow<'static, str>,
 }
 
 impl Prompt {
     pub(crate) const STYLE: Style = Style::new().bright_green().bold();
 
-    #[allow(unused)]
     pub(crate) fn new(text: impl Display) -> Self {
         let content = Cow::from(format!("{text}> "));
-        let pretty_content = content.terminal_style(Self::STYLE).to_string();
         Self {
-            _content: content,
-            pretty_content,
+            content,
         }
     }
 
     pub(crate) fn as_str(&self) -> &str {
-        &self.pretty_content
+        &self.content
     }
 }
 
 impl Default for Prompt {
     fn default() -> Self {
-        let content = Cow::from("> ");
-        let pretty_content = content.terminal_style(Self::STYLE).to_string();
         Self {
-            _content: content,
-            pretty_content,
+            content: Cow::from("> "),
         }
     }
 }
 
 impl Display for Prompt {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let Self { pretty_content, .. } = self;
-        write!(f, "{pretty_content}")
+        let Self { content, .. } = self;
+        write!(f, "{content}")
     }
 }
